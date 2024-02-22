06218e35b2dc8f961087c7712eb22f7e024ef1b3
samleticias <sammyaleticia09@gmail.com>
2024-02-21 22:47:06 -0300

    funcoes

--- lista2/2.maioremenor.js
+++ lista2/2.maioremenor.js
@@ -1,42 +1,42 @@
-import { question } from 'readline-sync';
+import { question } from 'readline-sync'
 
 function main() {
-  console.log('|**** NÚMEROS ****|');
+  console.log('|**** NÚMEROS ****|')
 
-  const n1 = get_number('Primeiro número: ');
-  const n2 = get_number('Segundo número: ');
+  const n1 = get_number('Primeiro número: ')
+  const n2 = get_number('Segundo número: ')
 
-  let maior = verificar_maior(n1, n2);
-  let menor = verificar_menor(n1, n2);
+  let maior = verificar_maior(n1, n2)
+  let menor = verificar_menor(n1, n2)
 
-  console.log('O maior número é ' + maior + ' e o menor número é ' + menor);
+  console.log('O maior número é ' + maior + ' e o menor número é ' + menor)
 }
 
 function get_number(texto) {
-  const numero = Number(question(texto));
-  return numero;
+  const numero = Number(question(texto))
+  return numero
 }
 
 function verificar_maior(n1, n2) {
-  let maior = 0;
+  let maior = 0
 
   if (n1 > n2) {
-    maior = n1;
+    maior = n1
   } else {
-    maior = n2;
+    maior = n2
   }
-  return maior;
+  return maior
 }
 
 function verificar_menor(n1, n2) {
-  let menor = 0;
+  let menor = 0
 
   if (n1 < n2) {
-    menor = n1;
+    menor = n1
   } else {
-    menor = n2;
+    menor = n2
   }
-  return menor;
+  return menor
 }
 
-main();
+main()
--- lista2/3.maior3numeros.js
+++ lista2/3.maior3numeros.js
@@ -2,33 +2,33 @@
 import { question } from 'readline-sync'
 
 function main() {
-    console.log('|**** NÚMEROS ****|');
+    console.log('|**** NÚMEROS ****|')
   
-    const n1 = get_number('Primeiro número: ');
-    const n2 = get_number('Segundo número: ');
-    const n3 = get_number('Terceiro número: ');
+    const n1 = get_number('Primeiro número: ')
+    const n2 = get_number('Segundo número: ')
+    const n3 = get_number('Terceiro número: ')
   
-    let maior = verificar_maior(n1, n2, n3);
+    let maior = verificar_maior(n1, n2, n3)
   
-    console.log('O maior número é ' + maior);
+    console.log('O maior número é ' + maior)
   }
   
   function get_number(texto) {
-    const numero = Number(question(texto));
-    return numero;
+    const numero = Number(question(texto))
+    return numero
   }
   
   function verificar_maior(n1, n2, n3) {
-    let maior = 0;
+    let maior = 0
   
     if (n1 > n2 && n1 > n3) {
-      maior = n1;
+      maior = n1
     } else if (n2 > n1 && n2 > n3){
       maior = n2
     } else {
         maior = n3
     }
-    return maior;
+    return maior
   }
   
-  main();
+  main()
